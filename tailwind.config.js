14d5e2395085e25f8c21415b0f80adddd2d85793
faturrokhman <faturrokhman.atung@gmail.com>
2022-02-18 20:59:21 +0700

    font poppins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins']
+      },
+      colors: {
+        primary: "#065F46", //emerald.800
+        'primary-hover': "#064E3B", //emerald.900
+        'ring': "#047857", //emerald.700
+        'text-color': "#222B2A", 
+        'text-body': "#52605F",
+        'body-white': '#F5FFFE', //putih
+      },
+    },
+    
   },
   plugins: [
     require('tailwind-scrollbar-hide'),
